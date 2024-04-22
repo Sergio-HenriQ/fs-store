218b222065c83da50d1bbada36a0ed31fed0b60d
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-04-22 20:54:51 -0300

    feat: add ProductImages component

---
+++ src/app/product/[slug]/components/product-images.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+interface ProductImagesProps {
+  name: string;
+  imagesUrl: string[];
+}
+
+const ProductImages = ({ imagesUrl, name }: ProductImagesProps) => {
+  const [currentImage, setCurrentImage] = useState(imagesUrl[0]);
+
+  const handleImageClick = (imageUrl: string) => {
+    setCurrentImage(imageUrl);
+  };
+  return (
+    <div className="flex flex-col">
+      <div className=" flex h-[380px] w-full  items-center justify-center bg-accent">
+        <Image
+          src={currentImage}
+          alt={name}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="max-h[70%] max-w[80%] h-[auto] w-[auto]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
+        {imagesUrl.map((imagesUrl) => (
+          <button
+            key={imagesUrl}
+            className={`flex h-[100px] items-center justify-center rounded-lg bg-accent ${
+              imagesUrl === currentImage &&
+              "border-2 border-solid border-primary"
+            }`}
+            onClick={() => handleImageClick(imagesUrl)}
+          >
+            <Image
+              src={imagesUrl}
+              alt={name}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            ></Image>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { prismaClient } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
 
 interface ProductDetailsPageProps {
   params: {
@@ -18,7 +19,11 @@ const ProductDetailsPage = async ({
   if (!product) {
     return null;
   }
-  return <h1>{product.name}</h1>;
+  return (
+    <div>
+      <ProductImages imagesUrl={product.imageUrls} name={product.name} />
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
