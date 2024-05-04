95269eb0b5f2ced439ed3625ca2880ae4b18dd83
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-05-04 19:21:16 -0300

    feat: add stripe checkout to cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -8,9 +8,21 @@ import { Separator } from "./separator";
 import PaymentInfo from "../payment-info";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "./button";
-
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
+
   return (
     <div className="flex h-full flex-col gap-8">
       <Badge
@@ -58,7 +70,12 @@ const Cart = () => {
         </div>
       )}
 
-      <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
+      <Button
+        className="mt-7 font-bold uppercase"
+        onClick={handleFinishPurchaseClick}
+      >
+        Finalizar compra
+      </Button>
     </div>
   );
 };
