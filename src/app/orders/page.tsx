d67affbbb4d1c1850764361755036c6099d9ecdf
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-05-27 20:50:44 -0300

    fix: dynamic orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -5,6 +5,8 @@ import OrderItem from "./components/order-Item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 const OrdersPage = async () => {
   const user = await getServerSession(authOptions);
 
